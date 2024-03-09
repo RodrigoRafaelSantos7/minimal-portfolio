153e7e1fac13cb8f07385d09f1135b2e060b1a35
RodrigoRafaelSantos7 <rodrigorafaelsantos7@icloud.com>
2024-03-09 12:04:40 +0000

    Fix bug in image path

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,7 +2,7 @@ export default [
   {
     title: 'Minimal Developer Portfolio',
     imgUrl:
-      'https://github.com/RodrigoRafaelSantos7/minimal-portfolio/blob/main/public/assets/dev-portfolio.jpg',
+      'https://github.com/RodrigoRafaelSantos7/minimal-portfolio/public/assets/dev-portfolio.jpg',
     stack: ['React', 'Vite', 'Tailwind', 'GitHub Pages'],
     link: 'https://rodrigorafaelsantos7.github.io/minimal-portfolio/',
   },
