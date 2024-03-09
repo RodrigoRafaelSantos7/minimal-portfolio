887f161928e224789add34331690a1453adb1478
RodrigoRafaelSantos7 <rodrigorafaelsantos7@icloud.com>
2024-03-09 12:37:30 +0000

    Update portfolio image file extension

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,7 +1,7 @@
 export default [
   {
     title: 'Minimal Developer Portfolio',
-    imgUrl: 'assets/dev-portfolio.jpg',
+    imgUrl: 'assets/dev-portfolio.png',
     stack: ['React', 'Vite', 'Tailwind', 'GitHub Pages'],
     link: 'https://rodrigorafaelsantos7.github.io/minimal-portfolio/',
   },
